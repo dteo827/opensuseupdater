b7e26ca8d78d171ef60b9e6aec82e16f48dc47c6
David <dteo827@gmail.com>
2015-04-04 23:09:25 -0400

    Update iptables.sh

--- iptables.sh
+++ iptables.sh
@@ -9,22 +9,34 @@
 # This scriptables is part of nixCraft shell scriptables collection (NSSC)
 # Visit http://bash.cyberciti.biz/ for more information.
 # -------------------------------------------------------------------------
-localIP=$(ip route show | awk '(NR == 2) {print $9}')
-echo $serverIP
-serverIP = sed 's/.$//' localIP
-echo $serverIP
+#make temp file for local ip
+read -p "what is the t in 10.10.10t.1 ? " t
+echo -n "10.10.10" >> localIP
+echo -n $t >> localIP
+echo -n ".1" >> localIP
+localIPaddr=$(<localIP)
+rm localIP
+echo $localIPaddr
 
-iptables="/sbin/iptablesables"
+#make temp file for server ip
+echo -n "10.10.10" >> serverIP
+echo -n $t >> serverIP
+echo -n ".15" >> serverIP
+echo "before cat"
+serverIPaddr=$(<serverIP)
+echo $serverIPaddr
+
+
 SPAMLIST="blockedip"
 SPAMDROPMSG="BLOCKED IP DROP"
  
 echo "Starting IPv4 Wall..."
-iptables -F
-iptables -X
-iptables -t nat -F
-iptables -t nat -X
-iptables -t mangle -F
-iptables -t mangle -X
+/usr/sbin/iptables -F
+/usr/sbin/iptables -X
+/usr/sbin/iptables -t nat -F
+/usr/sbin/iptables -t nat -X
+/usr/sbin/iptables -t mangle -F
+/usr/sbin/iptables -t mangle -X
 modprobe ip_conntrack
  
 [ -f /root/scriptabless/blocked.ips.txt ] && BADIPS=$(egrep -v -E "^#|^$" /root/scriptabless/blocked.ips.txt)
@@ -32,18 +44,18 @@ modprobe ip_conntrack
 PUB_IF="eth0"
  
 #unlimited 
-iptables -A INPUT -i lo -j ACCEPT
-iptables -A OUTPUT -o lo -j ACCEPT
+/usr/sbin/iptables -A INPUT -i lo -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -o lo -j ACCEPT
  
 # DROP all incomming traffic
-iptables -P INPUT DROP
-iptables -P OUTPUT DROP
-iptables -P FORWARD DROP
+/usr/sbin/iptables -P INPUT DROP
+/usr/sbin/iptables -P OUTPUT DROP
+/usr/sbin/iptables -P FORWARD DROP
  
 if [ -f /root/scriptabless/blocked.ips.txt ];
 then
 # create a new iptablesables list
-iptables -N $SPAMLIST
+/usr/sbin/iptables -N $SPAMLIST
  
 for ipblock in $BADIPS
 do
@@ -51,46 +63,46 @@ do
    iptables -A $SPAMLIST -s $ipblock -j DROP
 done
  
-iptables -I INPUT -j $SPAMLIST
-iptables -I OUTPUT -j $SPAMLIST
-iptables -I FORWARD -j $SPAMLIST
+/usr/sbin/iptables -I INPUT -j $SPAMLIST
+/usr/sbin/iptables -I OUTPUT -j $SPAMLIST
+/usr/sbin/iptables -I FORWARD -j $SPAMLIST
 fi
  
 # Block sync
-iptables -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Drop Sync"
-iptables -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW -j DROP
+/usr/sbin/iptables -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Drop Sync"
+/usr/sbin/iptables -A INPUT -i ${PUB_IF} -p tcp ! --syn -m state --state NEW -j DROP
  
 # Block Fragments
-iptables -A INPUT -i ${PUB_IF} -f  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fragments Packets"
-iptables -A INPUT -i ${PUB_IF} -f -j DROP
+/usr/sbin/iptables -A INPUT -i ${PUB_IF} -f  -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fragments Packets"
+/usr/sbin/iptables -A INPUT -i ${PUB_IF} -f -j DROP
  
 # Block bad stuff
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL ALL -j DROP
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL FIN,URG,PSH -j DROP
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL ALL -j DROP
  
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "NULL Packets"
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -j DROP # NULL packets
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "NULL Packets"
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL NONE -j DROP # NULL packets
  
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,RST SYN,RST -j DROP
  
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "XMAS Packets"
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP #XMAS
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "XMAS Packets"
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags SYN,FIN SYN,FIN -j DROP #XMAS
  
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fin Packets Scan"
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -j DROP # FIN packet scans
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -m limit --limit 5/m --limit-burst 7 -j LOG --log-level 4 --log-prefix "Fin Packets Scan"
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags FIN,ACK FIN -j DROP # FIN packet scans
  
-iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
+/usr/sbin/iptables  -A INPUT -i ${PUB_IF} -p tcp --tcp-flags ALL SYN,RST,ACK,FIN,URG -j DROP
  
 # Allow full outgoing connection but no incomming stuff
-iptables -A INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -o eth0 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+/usr/sbin/iptables -A INPUT -i eth0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -o eth0 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
  
 # Allow ssh 
-iptables -A INPUT -p tcp --destination-port 22 -j ACCEPT
+/usr/sbin/iptables -A INPUT -p tcp --destination-port 22 -j ACCEPT
  
 # allow incomming ICMP ping pong stuff
-iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+/usr/sbin/iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
+/usr/sbin/iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
  
 # Allow port 53 tcp/udp (DNS Server)
 #iptables -A INPUT -p udp --dport 53 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
@@ -100,16 +112,16 @@ iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j
 #iptables -A OUTPUT -p tcp --sport 53 -m state --state ESTABLISHED,RELATED -j ACCEPT
  
 # Open port 80
-iptables -A INPUT -p tcp --destination-port 80 -j ACCEPT
+/usr/sbin/iptables -A INPUT -p tcp --destination-port 80 -j ACCEPT
 ##### Add your rules below ######
  
 ##### END your rules ############
  
 # Do not log smb/windows sharing packets - too much logging
-iptables -A INPUT -p tcp -i eth0 --dport 137:139 -j REJECT
-iptables -A INPUT -p udp -i eth0 --dport 137:139 -j REJECT
+/usr/sbin/iptables -A INPUT -p tcp -i eth0 --dport 137:139 -j REJECT
+/usr/sbin/iptables -A INPUT -p udp -i eth0 --dport 137:139 -j REJECT
  
 # log everything else and drop
-iptables -A INPUT -j LOG
-iptables -A FORWARD -j LOG
-iptables -A INPUT -j DROP
+/usr/sbin/iptables -A INPUT -j LOG
+/usr/sbin/iptables -A FORWARD -j LOG
+/usr/sbin/iptables -A INPUT -j DROP
